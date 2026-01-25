71a98511cde49beede736b7170e9c1c6ee629f1d
Eduardo <66227987+Mestrian@users.noreply.github.com>
2026-01-25 17:41:48 -0300

    expanded search and added img loading placeholder

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 "use strict";
 
-
 const BASE_URL_TMDB = 'https://api.themoviedb.org/3';
 const IMG_URL_TMDB = 'https://image.tmdb.org/t/p/w500';
+let timerPesquisa; 
 
-const moviesWrapper = document.querySelector("#moviesWrapper");
+const trendingMoviesWrapper = document.querySelector("#trendingMoviesWrapper");
 const pesquisa = document.querySelector(".pesquisa");
 
-let mockMovies = []; 
+let trendingMovies = []; 
 
 // EVENT LISTENERS
 document.addEventListener("DOMContentLoaded", () => {
@@ -15,8 +15,14 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 pesquisa.addEventListener("input", (e) => {
-    const termoBusca = e.target.value.toLowerCase().trim(); 
-    filtrar(termoBusca, mockMovies, "titulo"); 
+    const termoBusca = e.target.value.toLowerCase().trim();
+    
+    clearTimeout(timerPesquisa);
+
+    timerPesquisa = setTimeout(() => {
+        pesquisarFilmes(termoBusca, trendingMovies, "titulo"); 
+    }, 350);
+
 });
 
 // CARREGAR DADOS
@@ -28,34 +34,59 @@ async function carregarFilmes() {
         
         const dados = await resposta.json(); 
 
-        mockMovies = dados.results.map(filme => ({
+        trendingMovies = dados.results.map(filme => ({
             titulo: filme.title,
             ano: filme.release_date ? filme.release_date.split("-")[0] : "N/A", 
             nota: filme.vote_average ? filme.vote_average.toFixed(1) : "0.0",
             urlImagem: filme.poster_path ? `${IMG_URL_TMDB}${filme.poster_path}` : "https://via.placeholder.com/500x750?text=Sem+Poster",
         }));
 
-        listarFilmes(mockMovies); 
+        listarFilmes(trendingMovies); 
 
     } catch (error) {
         console.error("Algo deu errado:", error);
-        moviesWrapper.innerHTML = "<p>Erro ao carregar os filmes</p>";
+        trendingMoviesWrapper.innerHTML = "<p>Erro ao carregar os filmes</p>";
     }
 }
 
-// FUNÇÕES CORE
-function filtrar(filtro, alvo, termo) { 
-    const Filtrados = alvo.filter(filme => {
-        return filme[termo].toLowerCase().includes(filtro);
-    });
-    listarFilmes(Filtrados); 
+async function pesquisarFilmes(termo){
+
+    if (!termo) {
+        listarFilmes(trendingMovies);
+        return;
+    }
+
+    try {
+        const url = `${BASE_URL_TMDB}/search/movie?api_key=${API_KEY_TMDB}&language=pt-BR&query=${termo}`;
+
+        const resposta = await fetch(url);
+
+        if(!resposta.ok) throw new Error('Erro ao se conectar a API'); 
+        
+        const dados = await resposta.json();
+        
+        const resultadosDaBusca = dados.results.map(filme=> ({
+            titulo: filme.title,
+            ano: filme.release_date ? filme.release_date.split("-")[0] : "N/A",
+            nota: filme.vote_average ? filme.vote_average.toFixed(1) : "0.0",
+            urlImagem: filme.poster_path ? `${IMG_URL_TMDB}${filme.poster_path}` : "https://via.placeholder.com/500x750?text=Sem+Poster"
+        }))
+
+        listarFilmes(resultadosDaBusca); 
+    }  catch (error) {
+        console.log ("Falha na pesquisa: ", error);
+    }
 }
 
+
+
+// FUNÇÕES CORE
+
 function listarFilmes(filmes) {
-    moviesWrapper.innerHTML = "";  
+    trendingMoviesWrapper.innerHTML = "";  
     filmes.forEach(filme => {
         const filmeCard = criarCard(filme);
-        moviesWrapper.append(filmeCard);
+        trendingMoviesWrapper.append(filmeCard);
     });
 }
 
@@ -63,15 +94,26 @@ function criarCard(filme) {
     const filmeCard = document.createElement("article");
     filmeCard.className = "cardContainer";
 
-    filmeCard.innerHTML = `
-            <picture class="moviePoster">
-                <img src="${filme.urlImagem}" alt="Poster de ${filme.titulo}" loading="lazy">
-            </picture>
+    const img = new Image();
+    img.src = filme.urlImagem;
+    img.alt = `Poster de ${filme.titulo}`;
 
+    const posterContainer = document.createElement("picture"); 
+    posterContainer.className = "moviePoster imgPlaceholder";
+
+    img.onload = () => {
+        posterContainer.classList.remove("imgPlaceholder");
+        posterContainer.appendChild(img);
+    }
+
+    filmeCard.innerHTML = `
             <section class="movieDesc">
                 <span class="movieName">${filme.titulo}</span>
                 <span class="releaseDate">${filme.ano}</span>
             </section>
     `;
+
+    filmeCard.prepend(posterContainer);
+    
     return filmeCard;
 }
